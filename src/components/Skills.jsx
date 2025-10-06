c32b9779ef7f26c30b5b554cf3f7ba08bfde1313
badr <badrhanafi82@gmail.com>
2025-10-06 12:02:59 +0100

    lastcommit 2

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -152,7 +152,7 @@ const Skills = () => {
 
   const ToolCard = ({ tool, idx }) => (
     <center>
-      {/* for connit o sf ! */}
+      
            <motion.div
       variants={scaleIn(idx * 0.08)}
       whileHover={{ scale: 1.08, rotate: -3 }}
