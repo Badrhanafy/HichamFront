0ccbe9a04b9ae5a2163d9e642302ba369cbf51f5
badr <badrhanafi82@gmail.com>
2025-10-07 01:12:44 +0100

    project not completed yet !!!!

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from './components/Footer'
 import axios from 'axios'
 import { Outlet } from 'react-router-dom'
 import { ScrollProgress, ScrollProgressProvider } from '@/components/animate-ui/primitives/animate/scroll-progress'
- axios.defaults.withCredentials = true;
+/*  axios.defaults.withCredentials = true; */
 function App() {
   return (
     <div className="App bg-gradient-to-br from-white to-gray-50 text-gray-800 min-h-screen">
