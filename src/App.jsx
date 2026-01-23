e83d82eea69588db2c6640ea97a3304b1b42cf4c
badr <badrhanafi82@gmail.com>
2026-01-23 22:31:40 +0100

    message for the restruction of the website

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,27 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import axios from 'axios'
 import { Outlet } from 'react-router-dom'
+import logo from './assets/images/logo.png'
 import { ScrollProgress, ScrollProgressProvider } from '@/components/animate-ui/primitives/animate/scroll-progress'
 /*  axios.defaults.withCredentials = true; */
 function App() {
   return (
     <div className="App bg-gradient-to-br from-white to-gray-50 text-gray-800 min-h-screen">
-      <ScrollProgressProvider global>
+      {/* <ScrollProgressProvider global>
         <ScrollProgress className="fixed top-0 left-0 w-full h-1 bg-purple-600 z-50" />
         <Navbar />
         
-        {/* Outlet: hna ghadi yban lcontent dial kol page */}
+    
         <main>
           <Outlet />
         </main>
 
         <Footer />
-      </ScrollProgressProvider>
+      </ScrollProgressProvider> */}
+     <img src={logo} />
+      <h1>Coming Soon</h1>
+<p>Website under maintenance</p>
+
     </div>
   )
 }
